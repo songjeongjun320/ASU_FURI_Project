f4156de6d3e278a8fe0f8bdf03dab36d9ddb68ba
Jun <songjeongjun320@gmail.com>
2024-07-19 07:31:21 -0700

    07/19/2024 - AWS Test - Jun Song

--- Test_AWS.py
+++ Test_AWS.py
@@ -2,24 +2,6 @@ from dotenv import load_dotenv
 import os
 import boto3
 
-def define_client():
-  client = boto3.client('textract',region_name='us-east-1',aws_access_key_id='AKIAYS2NWZIH6AMEEPG4',aws_secret_access_key='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')
-  return client
-
-def connect_s3(area, access_id, access_key):
-  try:
-    s3 = boto3.client(
-        service_name="s3",
-        region_name=area,
-        aws_access_key_id=access_id,
-        aws_secret_access_key=access_key,
-    )
-  except Exception as e:
-    print(e)
-  else:
-    print("s3 bucket connected!")
-    return s3
-
 def configure():
     load_dotenv()
 
@@ -28,6 +10,29 @@ def get_acct():
     access_id = os.getenv('access_id')
     access_key = os.getenv('access_key')
     return area, access_id, access_key
+
+def connect_s3(area, access_id, access_key):
+    try:
+        s3 = boto3.client(
+            service_name="s3",
+            region_name=area,
+            aws_access_key_id=access_id,
+            aws_secret_access_key=access_key,
+        )
+    except Exception as e:
+        print(e)
+    else:
+        print("s3 bucket connected!")
+        return s3
+  
+  # Extract the text from the cutted image file
+def extract_text(client):
+    #
+    with open('./backend/runsimg_cutted.jpg', 'rb') as image:
+        img = bytearray(image.read())
+
+    response = client.detect_document_text(Document={'Bytes': img})
+    return response
 
 def main():
     configure()
